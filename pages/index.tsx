c98088be456abc9241df199a154a67c524fa3229
Sasha Pimenov <pimenovs33@gmail.com>
2024-06-18 18:18:27 +0300

    Added Button component, svgr, modified config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
-import { Htag } from "../components";
+import { Button, Htag } from "../components";
 
 export default function Home() {
 	return (
 		<>
 			<Htag tag="h1">Text</Htag>
+			<Button appearance="primary" arrow="right">
+				Button
+			</Button>
+			<Button appearance="ghost" arrow="down">
+				Button
+			</Button>
 		</>
 	);
 }
