e6fc76bf278ab1cc0818641510e5dc650aee3515
Sasha Pimenov <pimenovs33@gmail.com>
2024-06-26 17:18:11 +0300

    Added adaptive layout for components Layout and Footer

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -3,17 +3,16 @@ import { Sidebar } from "./Sidebar/Sidebar";
 import { LayoutProps } from "./Layout.props";
 import { Footer } from "./Footer/Footer";
 import { FunctionComponent } from "react";
+import styles from "./Layout.module.css";
 
 const Layout = ({ children }: LayoutProps) => {
 	return (
-		<>
-			<Header />
-			<div>
-				<Sidebar />
-				<div>{children}</div>
-			</div>
-			<Footer />
-		</>
+		<div className={styles.wrapper}>
+			<Header className={styles.header} />
+			<Sidebar className={styles.sidebar} />
+			<div className={styles.body}>{children}</div>
+			<Footer className={styles.footer} />
+		</div>
 	);
 };
 
