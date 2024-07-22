9d60dcb11987d4ca01700c222a14f0207ff48c16
Sasha Pimenov <pimenovs33@gmail.com>
2024-07-22 15:56:54 +0300

    Added mobile menu

--- layout/Header/Header.tsx
+++ layout/Header/Header.tsx
@@ -1,5 +1,57 @@
+import cn from "classnames";
+import { motion } from "framer-motion";
 import { HeaderProps } from "./Header.props";
+import styles from "./Header.module.css";
+import Logo from "../logo.svg";
+import { ButtonIcon } from "@/components";
+import { Sidebar } from "../Sidebar/Sidebar";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
-export const Header = ({ ...props }: HeaderProps) => {
-	return <div {...props}>Header</div>;
+export const Header = ({ className, ...props }: HeaderProps) => {
+	const [isOpened, setIsOpened] = useState<boolean>(false);
+	const router = useRouter();
+
+	useEffect(() => {
+		setIsOpened(false);
+	}, [router]);
+
+	const variants = {
+		opened: {
+			opacity: 1,
+			x: 0,
+			transition: {
+				stiffness: 20,
+			},
+		},
+		closed: {
+			opacity: 0,
+			x: "100%",
+		},
+	};
+
+	return (
+		<header className={cn(className, styles.header)} {...props}>
+			<Logo />
+			<ButtonIcon
+				appearance="white"
+				icon="menu"
+				onClick={() => setIsOpened(true)}
+			/>
+			<motion.div
+				variants={variants}
+				initial="closed"
+				animate={isOpened ? "opened" : "closed"}
+				className={styles.mobileMenu}
+			>
+				<Sidebar />
+				<ButtonIcon
+					className={styles.menuClose}
+					appearance="white"
+					icon="close"
+					onClick={() => setIsOpened(false)}
+				/>
+			</motion.div>
+		</header>
+	);
 };
