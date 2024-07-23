d1de540ace0a9c870fea5b82f7f42bf4058e3d35
Sasha Pimenov <pimenovs33@gmail.com>
2024-07-23 19:23:43 +0300

    Added accessibility to Sort

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -6,7 +6,7 @@ import SortIcon from "./sort.svg";
 export const Sort = ({ sort, setSort, className, ...props }: SortProps) => {
 	return (
 		<div className={cn(styles.sort, className)} {...props}>
-			<span
+			<button
 				className={cn({
 					[styles.active]: sort === SortEnum.Rating,
 				})}
@@ -14,8 +14,8 @@ export const Sort = ({ sort, setSort, className, ...props }: SortProps) => {
 			>
 				<SortIcon className={styles.sortIcon} />
 				По рейтингу
-			</span>
-			<span
+			</button>
+			<button
 				className={cn({
 					[styles.active]: sort === SortEnum.Price,
 				})}
@@ -23,7 +23,7 @@ export const Sort = ({ sort, setSort, className, ...props }: SortProps) => {
 			>
 				<SortIcon className={styles.sortIcon} />
 				По цене
-			</span>
+			</button>
 		</div>
 	);
 };
