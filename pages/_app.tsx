f60875b5ec6b89967911ff93b40b629211fc4994
Sasha Pimenov <pimenovs33@gmail.com>
2024-07-28 20:15:54 +0300

    Added metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,20 @@ import Head from "next/head";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Noto_Sans } from "next/font/google";
+import ym, { YMInitializer } from "react-yandex-metrika";
+import { useEffect } from "react";
 
 const noto_sans = Noto_Sans({ subsets: ["cyrillic", "latin"] });
 
 export default function App({ Component, pageProps, router }: AppProps) {
+	useEffect(() => {
+		router.events.on("routeChangeComplete", (url: string) => {
+			if (typeof window !== "undefined") {
+				ym("hit", url);
+			}
+		});
+	}, []);
+
 	return (
 		<>
 			<style jsx global>{`
@@ -15,6 +25,7 @@ export default function App({ Component, pageProps, router }: AppProps) {
 				}
 			`}</style>
 			<Head>
+				<link rel="preconnect" href="https://mc.yandex.ru" />
 				<title>My top - наш лучший топ!</title>
 				<meta
 					name="description"
@@ -28,12 +39,14 @@ export default function App({ Component, pageProps, router }: AppProps) {
 					property="og:url"
 					content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}
 				/>
-				<meta
-					property="og:locale"
-					content="ru_RU"
-				/>
+				<meta property="og:locale" content="ru_RU" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+			<YMInitializer
+				accounts={[]}
+				options={{ webvisor: true, defer: true }}
+				version="2"
+			/>
 			<Component {...pageProps} />
 		</>
 	);
